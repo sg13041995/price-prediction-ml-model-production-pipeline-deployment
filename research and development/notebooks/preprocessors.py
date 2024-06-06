00de1dffa9ff1f5be2f2173e828814a117cd8d53
sg13041995 <subhankar.github.130495@gmail.com>
2024-06-06 13:47:37 +0530

    r&d model pipeline done

---
+++ research and development/notebooks/preprocessors.py
@@ -0,0 +1,62 @@
+# =========================================================
+# MODULE IMPORTS
+# =========================================================
+
+import numpy as np
+import pandas as pd
+
+# To inherit the scikit learn classes
+# To utilize the basic features provided by scikit learn classes
+# Make our class compatible with scikit learn  
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+
+class TemporalVariableTransformer(BaseEstimator, TransformerMixin):
+	# Temporal elapsed time transformer
+
+    def __init__(self, variables, reference_variable):
+        
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.variables = variables
+        self.reference_variable = reference_variable
+
+    def fit(self, X, y=None):
+        # we need this step to fit the sklearn pipeline
+        return self
+
+    def transform(self, X):
+
+    	# so that we do not over-write the original dataframe
+        X = X.copy()
+        
+        for feature in self.variables:
+            X[feature] = X[self.reference_variable] - X[feature]
+
+        return X
+
+
+
+# categorical missing value imputer
+class Mapper(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables, mappings):
+
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+
+        self.variables = variables
+        self.mappings = mappings
+
+    def fit(self, X, y=None):
+        # we need the fit statement to accomodate the sklearn pipeline
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.mappings)
+
+        return X
